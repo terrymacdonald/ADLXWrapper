9ce86b24913f288ecc4bd587750e5f5071fdaa1e
terrymacdonald <terry.macdonald@cosive.com>
2025-11-16 11:48:31 +1300

    Updated the version.

--- ADLXWrapper.Tests/cs_bindings/ADLXVersion.cs
+++ ADLXWrapper.Tests/cs_bindings/ADLXVersion.cs
@@ -1,6 +1,6 @@
 ﻿//
 // Auto-generated version file - DO NOT EDIT MANUALLY
-// Generated on: 2025-11-16T10:44:44Z
+// Generated on: 2025-11-16T11:47:06Z
 // Build system: ADLXWrapper version management
 //
 // Copyright (c) 2025 Terry MacDonald. All rights reserved.
@@ -26,26 +26,26 @@ namespace ADLXWrapper
         /// <summary>
         /// Patch version number (bug fixes, auto-incremented)
         /// </summary>
-        public const int Patch = 48;
+        public const int Patch = 50;
         
         /// <summary>
         /// Full version string in SemVer format (Major.Minor.Patch)
         /// </summary>
-        public const string Version = "1.0.48";
+        public const string Version = "1.0.50";
         
         /// <summary>
         /// Build date in ISO 8601 format
         /// </summary>
-        public const string BuildDate = "2025-11-16T10:44:44Z";
+        public const string BuildDate = "2025-11-16T11:47:06Z";
         
         /// <summary>
         /// Git commit hash (short form)
         /// </summary>
-        public const string GitCommit = "327cb98";
+        public const string GitCommit = "bc37099";
         
         /// <summary>
         /// Full version information string
         /// </summary>
-        public const string FullVersionInfo = "ADLXWrapper v1.0.48 (build 2025-11-16T10:44:44Z, commit 327cb98)";
+        public const string FullVersionInfo = "ADLXWrapper v1.0.50 (build 2025-11-16T11:47:06Z, commit bc37099)";
     }
 }
