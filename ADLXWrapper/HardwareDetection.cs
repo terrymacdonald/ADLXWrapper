3b059b83e9542c58af3423692697f6e060c0b97e
Terry MacDonald <terry.macdonald@cosive.com>
2025-12-12 09:18:01 +1300

    Migrated Hardware Detection to the main ADLXWrapper

--- ADLXWrapper/HardwareDetection.cs
+++ ADLXWrapper/HardwareDetection.cs
@@ -2,26 +2,25 @@ using System;
 using System.Management;
 using System.Runtime.Versioning;
 
-namespace ADLXWrapper.Tests
+namespace ADLXWrapper
 {
     /// <summary>
-    /// Hardware detection utilities for AMD GPU presence
+    /// Hardware detection utilities for AMD GPU presence.
     /// </summary>
     public static class HardwareDetection
     {
         private const string AMD_PCI_VENDOR_ID = "1002";
 
         /// <summary>
-        /// Checks if an AMD GPU is present in the system via PCI Vendor ID detection
+        /// Checks if an AMD GPU is present in the system via PCI Vendor ID detection.
         /// </summary>
-        /// <param name="errorMessage">Details about why AMD GPU was not detected</param>
-        /// <returns>True if AMD GPU hardware is detected, false otherwise</returns>
+        /// <param name="errorMessage">Details about why AMD GPU was not detected.</param>
+        /// <returns>True if AMD GPU hardware is detected, false otherwise.</returns>
         [SupportedOSPlatform("windows")]
         public static bool HasAMDGPU(out string errorMessage)
         {
             try
             {
-                // Query for video controllers with AMD's PCI Vendor ID (1002)
                 using var searcher = new ManagementObjectSearcher(
                     $"SELECT * FROM Win32_VideoController WHERE PNPDeviceID LIKE 'PCI\\\\VEN_{AMD_PCI_VENDOR_ID}%'");
 
@@ -43,9 +42,9 @@ namespace ADLXWrapper.Tests
         }
 
         /// <summary>
-        /// Gets detailed information about detected AMD GPUs
+        /// Gets detailed information about detected AMD GPUs.
         /// </summary>
-        /// <returns>Array of GPU names, or empty array if none found</returns>
+        /// <returns>Array of GPU names, or empty array if none found.</returns>
         [SupportedOSPlatform("windows")]
         public static string[] GetAMDGPUNames()
         {
