679726cddc9a6472316f5bfe609f0a051e8ef00e
terrymacdonald <terry.macdonald@cosive.com>
2025-12-30 15:38:32 +1300

    Tweaks to allow merge

--- ADLXWrapper.NativeTests/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using Xunit;
-
-// ADLX hardware-backed calls are not thread-safe; run tests sequentially to avoid driver deadlocks or host crashes.
-[assembly: CollectionBehavior(DisableTestParallelization = true, MaxParallelThreads = 1)]
